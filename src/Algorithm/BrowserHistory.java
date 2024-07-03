6d1d55da256259a38a4764db50307988dbec8400
narumi <sychoi2008@naver.com>
2024-07-03 20:04:57 +0900

    Solved BrowserHistory

--- src/Algorithm/BrowserHistory.java
+++ src/Algorithm/BrowserHistory.java
@@ -3,43 +3,53 @@ package Algorithm;
 import java.util.LinkedList;
 
 class BrowserHistory {
-    String homepage;
-    LinkedList<String> pages;
-
-    // 현재 페이지를 가르키는 변수
-    int current_index;
-
+    private LinkedList<String> linkedList = new LinkedList<>();
+    int index = 0;
     public BrowserHistory(String homepage) {
-        this.homepage = homepage;
-        pages = new LinkedList<>();
-        pages.add(homepage);
-        current_index = 0;
+        linkedList.add(homepage);
     }
 
     public void visit(String url) {
-        if(current_index == pages.size() - 1) {
-            pages.add(url);
-            current_index++;
-        } else {
-            current_index++;
-            pages.set(current_index, url);
+        index++;
+        //linked list의 method 중 add는 인덱스를 지정하여 중간 삽입, 삭제가 가능함
+        linkedList.add(index, url);
+        // index + 1 = 진짜 원하는 linked list의 size
+        // 근데, 그 원하는 사이즈보다 현재 linked list의 size가 더 클 동안
+        // 언젠간 linked list = index + 1이 될 것임
+        while(index+1 < linkedList.size()) {
+            linkedList.removeLast();
         }
     }
 
     public String back(int steps) {
-        while(steps > 0 && current_index != 0) {
-            current_index--;
-            steps--;
+        // while문을 쓰지 않고 그냥 빼기해서 바로 접근
+        int temp_index = index - steps;
+        // 뺀 값이 0보다 작다면 맨 앞까지 가버린 것
+        if(temp_index < 0) {
+            // index를 0으로 바꾸고
+            index = 0;
+            // 0번째 인덱스 값 출력
+            return linkedList.get(0);
         }
-        return pages.get(current_index);
+        // 뺐을 때 아직 linked list 안에 있는 index 값이라면
+        // 그 값으로 초기화
+        index = temp_index;
+        // 출력
+        return linkedList.get(index);
+
     }
 
     public String forward(int steps) {
-        while(steps > 0 && current_index != pages.size()-1) {
-            current_index++;
-            steps--;
+        // 앞으로 가는 것도 마찬가지로 while문을 쓰지 않고 그냥 더하기로 바로 접근
+        int temp_index = index+steps;
+        // 하지만 더했을 때, linked list의 마지막 인덱스 값을 초과한다면 마지막 요소를 출력
+        if(temp_index > linkedList.size()-1) {
+            return linkedList.getLast();
         }
-        return pages.get(current_index);
+        // 아닐 경우는 index 값 업데이트 후 출력
+        index = temp_index;
+        return linkedList.get(index);
+
     }
 
     public static void main(String[] args) {
@@ -54,6 +64,9 @@ class BrowserHistory {
         System.out.println(browserHistory.forward(2));
         System.out.println(browserHistory.back(2));
         System.out.println(browserHistory.back(7));
+        browserHistory.visit("naver.com");
+        System.out.println(browserHistory.forward(5));
+
     }
 }
 
