dc616ab4b8cdab9c677d5e96b56bcc3f84af1151
narumi <sychoi2008@naver.com>
2024-07-10 21:01:44 +0900

    solved hash, palindrome

--- src/Algorithm/HashTest.java
+++ src/Algorithm/HashTest.java
@@ -1,25 +1,35 @@
 package Algorithm;
 
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class HashTest {
-    public static HashMap<Integer, Boolean> hm = new HashMap<>();
-    public static boolean checkCode(int [] nums, int target) {
-        for(int num : nums) {
-            hm.put(num, true);
+    public static HashMap<Integer, Integer> hm = new HashMap<>();
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap <Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            hm.put(nums[i], i);
         }
 
-        for(int num : nums) {
-            int needed_num = target - num;
-            if(hm.containsKey(needed_num)) {
-                return true;
+        for(int j=0; j<nums.length; j++) {
+            int needed_num = target - nums[j];
+            if(hm.containsKey(needed_num) && hm.get(needed_num)!=j) {
+                if(j < hm.get(needed_num)) {
+                    return new int[]{j,hm.get(needed_num)};
+                } else {
+                    return new int[]{hm.get(needed_num), j};
+                }
             }
         }
-        return false;
+        return new int[]{-1, -1};
     }
     public static void main(String[] args) {
         int [] nums = {4,1,9,7,8,2};
         int target = 14;
-        System.out.println(checkCode(nums, target));
+        for(int num : twoSum(nums, target)) {
+            System.out.print(num + " ");
+        };
+        StringTokenizer st = new StringTokenizer("h i");
+        System.out.println(st.countTokens());
     }
 }
