d7c7b03e3d2303637fe5ebff73f32ba24ceafde0
narumi <sychoi2008@naver.com>
2024-08-03 16:16:31 +0900

    greedy and brute force last solve

---
+++ src/Algorithm/BOJ/Snow.java
@@ -0,0 +1,47 @@
+package Algorithm.BOJ;
+
+import java.util.*;
+import java.io.*;
+public class Snow {
+    static boolean flag = false;
+    static int [] people;
+    static boolean [] visited;
+    static int [] answer;
+    public static void solution(int sum, int index) {
+        if(index == 7) {
+            if(sum == 100) {
+                Arrays.sort(answer);
+                for(int num : answer) {
+                    System.out.println(num);
+                }
+                flag = true;
+                return;
+            }
+            else return;
+
+        }
+
+        for(int i=0; i<9; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                answer[index] = people[i];
+                solution(sum+people[i], index+1);
+                if(flag == true) return;
+                visited[i] = false; // 원래대로
+
+            }
+        }
+    }
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        people = new int[9];
+        visited = new boolean[9];
+        answer = new int[7];
+
+        for(int i=0; i<9; i++) {
+            people[i] = Integer.parseInt(br.readLine());
+        }
+
+        solution(0, 0);
+    }
+}
---
+++ src/Algorithm/BOJ/Water.java
@@ -0,0 +1,38 @@
+package Algorithm.BOJ;
+
+import java.util.*;
+import java.io.*;
+public class Water {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+
+        int spots = Integer.parseInt(st.nextToken());
+        int tape = Integer.parseInt(st.nextToken());
+
+        int [] spot = new int [spots];
+        str = br.readLine();
+        st = new StringTokenizer(str);
+        for(int i=0; i<spots; i++) {
+            spot[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(spot);
+
+        int start = spot[0];
+        int end = spot[0] + (tape-1);
+
+        int answer = 1;
+
+        for(int next : spot) {
+            if(start<=next && next <= end) continue;
+            else {
+                start = next;
+                end = next+(tape-1);
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
