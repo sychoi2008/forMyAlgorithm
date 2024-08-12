4c042d97ca023788fa1892f33e1ddb97a75d428d
narumi <sychoi2008@naver.com>
2024-08-12 21:09:35 +0900

    brute force and backtracking practice

---
+++ src/Algorithm/BOJ/DiffIsBig.java
@@ -0,0 +1,60 @@
+package Algorithm.BOJ;
+
+import java.util.*;
+import java.io.*;
+
+public class DiffIsBig
+{
+    static int [] original;
+    static boolean [] visited;
+    static int [] temp;
+    static int num;
+
+    static int max_value = Integer.MIN_VALUE;
+
+    public static void getResult() {
+        int res =  0;
+
+        for(int i=0; i<num-1; i++) {
+            res = res + Math.abs(temp[i]-temp[i+1]);
+        }
+
+        max_value = Math.max(max_value, res);
+    }
+
+    public static void changeNum (int index) {
+        if(index == num) {
+            getResult();
+            return;
+        }
+
+        for(int i=0; i<num; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                temp[index] = original[i];
+                changeNum(index+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        num = Integer.parseInt(br.readLine());
+
+        original = new int [num];
+        visited = new boolean [num];
+        temp = new int [num];
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+
+        for(int i=0; i<num; i++) {
+            original[i] = Integer.parseInt(st.nextToken());
+        }
+
+        changeNum(0);
+
+        System.out.println(max_value);
+    }
+}
---
+++ src/Algorithm/BOJ/PermutationChange.java
@@ -0,0 +1,42 @@
+package Algorithm.BOJ;
+
+import java.util.*;
+import java.io.*;
+
+public class PermutationChange {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int printRange = N-K;
+
+        int [] nums = new int [N];
+
+        str = br.readLine();
+        st = new StringTokenizer(str,",");
+
+        for(int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int len = nums.length - 1;
+
+        while(K!=0) {
+            for(int i=1; i<=len; i++) {
+                nums[i-1] = nums[i] - nums[i-1];
+            }
+            K--;
+            len--;
+        }
+
+        for(int i=0; i<printRange; i++) {
+            if(i==printRange-1) System.out.print(nums[i]);
+            else System.out.print(nums[i]+",");
+
+        }
+
+    }
+}
